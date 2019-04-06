570ae9e969095c9c08f19996c8442a816c075fe6
Caio Vidal <caio.negreiros@ccc.ufcg.edu.br>
2019-04-05 23:48:37 -0300

    ok

--- ri_lab_01/spiders/oantagonista.py
+++ ri_lab_01/spiders/oantagonista.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json
+from datetime import datetime
 
 from ri_lab_01.items import RiLab01Item
 from ri_lab_01.items import RiLab01CommentItem
-
 
 class OantagonistaSpider(scrapy.Spider):
     name = 'oantagonista'
     allowed_domains = ['oantagonista.com']
     start_urls = []
+
+    data_limite = datetime.strptime('01/01/2018', '%d/%m/%Y')
 
     def __init__(self, *a, **kw):
         super(OantagonistaSpider, self).__init__(*a, **kw)
@@ -17,15 +19,36 @@ class OantagonistaSpider(scrapy.Spider):
                 data = json.load(json_file)
         self.start_urls = list(data.values())
 
+
     def parse(self, response):
-        #
-        # inclua seu código aqui
-        #
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
-        #
-        #
-        #
+        for art in response.css('div.collect article'):
+            data = art.css('a.article_link span.postmeta time::attr(datetime)').get()
+            data = datetime.strptime(data, '%Y-%m-%d %H:%M:%S')
+            
+            if (self.data_limite <= data):
+                titulo = art.css('a.article_link::attr(title)').get()
+                data_noticia = data.strftime("%d/%m/%Y %H:%M:%S")
+                secao = art.css('a.article_link span.postmeta span.categoria::text').get()
+                texto = art.css('a.article_link p').get()
+                url = art.css('a.article_link::attr(href)').get()
+
+                yield {
+                    'titulo': titulo,
+                    'subtitulo': '-',
+                    'autor': '-',
+                    'data': data_noticia,
+                    'secao': secao,
+                    'texto': texto,
+                    'url': url
+                }
+            else:
+                quit()                
+                
+        listUrl = response.url.split('/')
+        
+        if len(listUrl) == 6:
+            newPage = listUrl[0] + '//' + listUrl[2] + '/' + listUrl[3] + '/' + str(int(listUrl[4]) + 1) + '/'
+        else:
+            newPage = listUrl[0] + '//' + listUrl[2] + '/' + 'pagina' + '/' + '2' + '/'
+                                          
+        yield scrapy.Request(url=newPage, callback=self.parse)
